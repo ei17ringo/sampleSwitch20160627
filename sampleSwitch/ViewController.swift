1feba8c865bb7bc7e382e7535bf7b63a679e4c48
Eriko Ichinohe <ei17ringo@gmail.com>
2016-07-20 15:50:34 +0800

    init commit

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapSwitch(sender: UISwitch) {
+        
+        print("switchが押された\(sender.on)")
+        //TODO:ラベルを一つ追加して、スイッチがONのときには、「ONです」OFFのときは「OFFです」と表示しましょう(ヒント：if文を使用)
+        if sender.on == true {
+            myLabel.text = "ONです"
+        }else{
+            myLabel.text = "OFFです"
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
